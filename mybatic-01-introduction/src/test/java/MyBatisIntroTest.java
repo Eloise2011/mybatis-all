a19081ae29adfd4d71c0637b1d9181c43926a70e
Eloise2011 <joshuahbrooks@163.com>
2024-05-15 00:35:05 +0800

    2nd commit - MyBatis Demo Optimized

--- mybatic-01-introduction/src/test/java/MyBatisIntroTest.java
+++ mybatic-01-introduction/src/test/java/MyBatisIntroTest.java
@@ -13,14 +13,33 @@ import java.io.InputStream;
  */
 
 public class MyBatisIntroTest {
-    public static void main(String[] args) throws IOException {
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        SqlSessionFactoryBuilder ssfb = new SqlSessionFactoryBuilder();
-        SqlSessionFactory ssf = ssfb.build(is);
-        SqlSession sqlSession = ssf.openSession();// 如果使用的事务管理：是JDBC的话，底层实际上会执行：conn。setAutoCommit（false);
-        //SqlSession sqlSession = ssf.openSession(true);// 这种方式是不开启事务，也就是说任意DML一旦执行就会立即commit。因此该方式不被推崇
-        int count = sqlSession.insert("insertCar");
-        sqlSession.commit();
-        System.out.println("Rows affected = " + count);
+    public static void main(String[] args)  {
+        SqlSession sqlSession = null;
+        InputStream is = null;
+        try {
+            is = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            sqlSession = sqlSessionFactory.openSession();// 如果使用的事务管理：是JDBC的话，底层实际上会执行：conn.setAutoCommit（false);
+            //sqlSession = sqlSessionFactory.openSession(true);// 这种方式是不开启事务，也就是说任意DML一旦执行就会立即commit。因此该方式不被推崇
+            //biz sql execution
+            sqlSession.insert("insertCar");
+            //commit
+            int count = sqlSession.insert("insertCar");
+            System.out.println("Rows affected = " + count);
+            sqlSession.commit();
+        } catch (Exception e) {
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+            throw new RuntimeException(e);
+        }  finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+
+
+
     }
 }
